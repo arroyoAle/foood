2b67c974cd9c2a38478aa7bcdc1dc8232e53b07e
Alejandro Arroyo <41422777+arroyoAle@users.noreply.github.com>
2025-10-22 19:30:39 +0100

    Adding some new pages and initial partials (#1)

---
+++ lib/pages/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:spin_wheel/partials/drawer.dart';
+import 'package:spin_wheel/partials/top_bar.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  final String title = 'Flutter Demo Home Page';
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TopBarPartial(title: widget.title),
+      drawer: DrawerPartial(currentPage: 'home_page'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/pages/spin_wheel.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:spin_wheel/partials/drawer.dart';
+import 'package:spin_wheel/partials/top_bar.dart';
+
+class SpinWheelPage extends StatefulWidget {
+
+  final String title = 'Spin Wheel Page';
+  @override
+  State<SpinWheelPage> createState() => _SpinWheelPageState();
+}
+
+class _SpinWheelPageState extends State<SpinWheelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TopBarPartial(title: widget.title),
+      drawer: DrawerPartial(currentPage: 'spinPage'),
+    );
+  }
+}
