2f19ea8d3ca7b7b10eb03bbed4a15fc1d8ce8ccc
arroyoAle <41422777+arroyoAle@users.noreply.github.com>
2025-12-09 20:11:13 +0000

    adding new home pages

---
+++ lib/pages/lists.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:spin_wheel/partials/drawer.dart';
+import 'package:spin_wheel/partials/top_bar.dart';
+
+class ListsHomePage extends StatefulWidget {
+  const ListsHomePage({super.key});
+
+  final String title = 'Spin Wheel Page';
+  @override
+  State<ListsHomePage> createState() => _ListsHomePageState();
+}
+
+class _ListsHomePageState extends State<ListsHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TopBarPartial(title: widget.title),
+      drawer: DrawerPartial(currentPage: 'spinPage'),
+    );
+  }
+}
---
+++ lib/pages/recipes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:spin_wheel/partials/drawer.dart';
+import 'package:spin_wheel/partials/top_bar.dart';
+
+class RecipesHomePage extends StatefulWidget {
+  const RecipesHomePage({super.key});
+
+  final String title = 'Spin Wheel Page';
+  @override
+  State<RecipesHomePage> createState() => _RecipesHomePageState();
+}
+
+class _RecipesHomePageState extends State<RecipesHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TopBarPartial(title: widget.title),
+      drawer: DrawerPartial(currentPage: 'spinPage'),
+    );
+  }
+}
