34b2e3f9fe9e829cd47750f32c2868042d3eb15a
arroyoAle <41422777+arroyoAle@users.noreply.github.com>
2025-12-27 17:15:50 +0100

    adding storage manager tests

--- test/run_test.dart
+++ test/run_test.dart
@@ -1,10 +1,12 @@
 import 'unit_tests/storage_unit_tests.dart' as storage_tests;
+import 'unit_tests/shopping_list_manager_tests.dart' as shopping_list_manager_tests;
 import 'widget_test/drawer_widget_tests.dart' as drawer_tests;
 import 'widget_test/lists_widget_tests.dart' as lists_tests;
 
 void main () {
   // Unit Tests
   storage_tests.main();
+  shopping_list_manager_tests.main();
 
   // Widget Tests
   drawer_tests.main();
