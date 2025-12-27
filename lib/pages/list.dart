8f9054195f2159d7b1b8b803480147b6db256304
arroyoAle <41422777+arroyoAle@users.noreply.github.com>
2025-12-27 16:36:03 +0100

    adding makefile and some more cleanup

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -8,7 +8,6 @@ class ListPage extends StatefulWidget {
   const ListPage({super.key, required this.manager});
 
   final ShoppingListManager manager;
-  // final String title = 'Lists Home Page';
 
   @override
   State<ListPage> createState() => _ListPageState();
@@ -40,59 +39,35 @@ class _ListPageState extends State<ListPage> {
     return Scaffold(
       appBar: TopBarPartial(title: shoppingList.name),
       drawer: DrawerPartial(currentPage: 'lists_page'),
-      body:
-      // FutureBuilder(
-      //   future: _loadingFuture,
-      //   builder: (context, snapshot) {
-      //     // --- While Loading ---
-      //     if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return const Center(child: CircularProgressIndicator());
-      //     }
-      //
-      //     // --- If an Error Occurs ---
-      //     if (snapshot.hasError) {
-      //       return Center(
-      //         child: Text('Error loading list: ${snapshot.error}')
-      //       );
-      //     }
-      //
-      //     // --- When Data is Loaded Successfully ---
-      //     // Now it's safe to access the shopping list from the manager
-      //     final shoppingList = _shoppingListManager.activeList!;
-      //
-      //     if (shoppingList.items.isEmpty) {
-      //       return const Center(
-      //         child: Text(
-      //           "No items in this list. \n\nAdd a new item with the '+' button below.",
-      //           textAlign: TextAlign.center,
-      //         ),
-      //       );
-      //     } else {
-      //       return
-              ListView.builder(
-              itemCount: shoppingList.items.length,
-              itemBuilder: (context, index) =>
-                Card(
-                  child: CheckboxListTile(
-                    value: shoppingList.items[index].selected,
-                    title: Text(shoppingList.items[index].name),
-                    secondary: Text(
-                        '${shoppingList.items[index]
-                            .quantity} ${shoppingList
-                            .items[index].units}'),
-                    onChanged: (bool? value) {
-                      setState(() {
-                        shoppingList.items[index].selected = value!;
-                        _shoppingListManager.saveActiveList();
-                      });
-                    },
-                    controlAffinity: ListTileControlAffinity.leading,
-                  ),
-                ),
+      body: shoppingList.items.isEmpty?
+        Center(
+          child: Text(
+            "No items in this list. \n\nAdd a new item with the '+' button below.",
+            textAlign: TextAlign.center,
+          ),
+        )
+          :
+        ListView.builder(
+          itemCount: shoppingList.items.length,
+          itemBuilder: (context, index) =>
+            Card(
+              child: CheckboxListTile(
+                value: shoppingList.items[index].selected,
+                title: Text(shoppingList.items[index].name),
+                secondary: Text(
+                    '${shoppingList.items[index]
+                        .quantity} ${shoppingList
+                        .items[index].units}'),
+                onChanged: (bool? value) {
+                  setState(() {
+                    shoppingList.items[index].selected = value!;
+                    _shoppingListManager.saveActiveList();
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
             ),
-          // }
-        // },
-      // ),
+        ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addNewItem,
         tooltip: 'Add new item',
