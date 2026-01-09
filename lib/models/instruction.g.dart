26144cb34010b8aa57de7e2cea216dddb4f26e03
arroyoAle <41422777+arroyoAle@users.noreply.github.com>
2026-01-09 22:25:10 +0000

    adding needed generated files

---
+++ lib/models/instruction.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'instruction.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Instruction _$InstructionFromJson(Map<String, dynamic> json) =>
+    Instruction(id: json['id'] as String, text: json['text'] as String);
+
+Map<String, dynamic> _$InstructionToJson(Instruction instance) =>
+    <String, dynamic>{'id': instance.id, 'text': instance.text};
