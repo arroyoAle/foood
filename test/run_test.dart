2354fda271ba12cb820b4bed55b3bc95e0483fcd
arroyoAle <41422777+arroyoAle@users.noreply.github.com>
2026-05-09 15:33:16 +0100

    silencing warning for tests

--- test/run_test.dart
+++ test/run_test.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'unit_tests/shopping_repository_test.dart' as shopping_repo_tests;
 import 'unit_tests/recipe_repository_test.dart' as recipe_repo_tests;
 import 'widget_test/drawer_widget_tests.dart' as drawer_tests;
@@ -5,6 +6,10 @@ import 'widget_test/lists_widget_tests.dart' as lists_tests;
 import 'widget_test/all_lists_widget_tests.dart' as all_list_tests;
 
 void main () {
+  // Drift generates a warning if we create multiple database instances.
+  // In tests, we do this intentionally (one per test).
+  driftRuntimeOptions.dontWarnAboutMultipleDatabases = true;
+
   // Unit Tests
   shopping_repo_tests.main();
   recipe_repo_tests.main();
